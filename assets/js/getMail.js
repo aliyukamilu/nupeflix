da4de46519845f3e54a30a7a87451d620dd56a54
unknown <deen.aliyu40@gmail.com>
2022-11-20 20:35:54 +0100

    second stuff

--- assets/js/getMail.js
+++ assets/js/getMail.js
@@ -15,3 +15,17 @@ $("#next1").click(function(){
   document.getElementById("baner").style.display = "none";
   document.getElementById("baner2").style.display = "flex";
 })
+
+
+$("#next2").click(function(){
+  document.getElementById("baner").style.display = "none";
+  document.getElementById("baner2").style.display = "none";
+  document.getElementById("baner3").style.display = "flex";
+})
+
+$("#next3").click(function(){
+  document.getElementById("baner").style.display = "none";
+  document.getElementById("baner2").style.display = "none";
+  document.getElementById("baner3").style.display = "none";
+  document.getElementById("baner4").style.display = "flex";
+})
